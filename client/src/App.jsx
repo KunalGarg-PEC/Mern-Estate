6f3a573d2440646bc4dd6a4924e6bf584ab809c7
kunal garg <gargkunal369@gmail.com>
2024-04-02 15:34:31 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
 
 export default function App() {
   return (
-    <h1>app</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={< About/>}></Route>
+      <Route path="/profile" element={< Profile/>}></Route>
+      <Route path="/signin" element={< Signin/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
